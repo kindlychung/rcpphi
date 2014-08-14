cf536e4daa7a02caf5184eba4bf7288b4cb221b4
Kaiyin Zhong <kindlychung@gmail.com>
2014-08-14 15:09:36 +0200

    a

--- src/rcpp_hello_world.cpp
+++ src/rcpp_hello_world.cpp
@@ -2,10 +2,16 @@
 #include <Rcpp.h>
 using namespace Rcpp;
 
+//' Example function of rcpp
+//'
+//' @param string first string
+//' @param string second string
+//' @return list list of two vectors
+//' @export
 // [[Rcpp::export]]
-List rcpp_hello_world() {
+List rcpp_hello_world(std::string a, std::string b) {
 
-    CharacterVector x = CharacterVector::create( "foo", "bar" )  ;
+    CharacterVector x = CharacterVector::create(a, b)  ;
     NumericVector y   = NumericVector::create( 0.0, 1.0 ) ;
     List z            = List::create( x, y ) ;
 
